b8fa55280a04ad2f788845a245c08747cfdc21ae
Spin <>
2025-07-08 09:21:24 +0100

    fix: new config

--- init.lua
+++ init.lua
@@ -1,2 +1,140 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- Configs
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.mouse = 'a'
+-- TODO: change this when using new status line
+-- vim.o.showmode = false
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.inccommand = 'split'
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.confirm = true
+
+-- Keybinds
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- Package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Packages
+require("lazy").setup({
+  spec = {
+    -- Theme
+    {
+      "ellisonleao/gruvbox.nvim",
+      priority = 1000,
+      lazy = false,
+      config = function()
+        vim.cmd.colorscheme("gruvbox")
+      end,
+    },
+    -- Which-key
+    {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts = {},
+      keys = {
+        {
+          "<leader>?",
+          function()
+            require("which-key").show({ global = false })
+          end,
+          desc = "Buffer Local Keymaps (which-key)",
+        },
+      },
+    },
+    -- LSP
+    {
+      "neovim/nvim-lspconfig",
+      dependencies = {
+        {
+          "mason-org/mason.nvim",
+          build = ":MasonUpdate",
+          cmd = { "Mason", "MasonUpdate", "MasonLog", "MasonInstall", "MasonUninstall", "MasonUninstallAll" },
+          config = true,
+        },
+        {
+          "mason-org/mason-lspconfig.nvim",
+          config = true,
+          keys = {
+            { "<C-space>", "<cmd>lua vim.lsp.completion.get()  <CR>", mode = "i" },
+            { "gh",        "<cmd>lua vim.lsp.buf.hover()       <CR>" },
+            { "gd",        "<cmd>lua vim.lsp.buf.definition()  <CR>" },
+            { "gD",        "<cmd>lua vim.lsp.buf.declaration() <CR>" },
+          },
+        },
+      },
+    },
+    -- Autocomplete
+    {
+      "Saghen/blink.cmp",
+      version = '1.*',
+      opts = {
+        keymap = { preset = 'default' },
+        appearance = {
+          nerd_font_variant = 'mono'
+        },
+        completion = { documentation = { auto_show = false } },
+        sources = {
+          default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+        fuzzy = { implementation = "lua" }
+      },
+      opts_extend = { "sources.default" }
+    },
+    -- Search
+    {
+      "ibhagwan/fzf-lua"
+    },
+    -- Indents length detection
+    {
+      'NMAC427/guess-indent.nvim',
+      config = true,
+    },
+  },
+  install = { colorscheme = { "gruvbox" } },
+  checker = { enabled = true },
+})
