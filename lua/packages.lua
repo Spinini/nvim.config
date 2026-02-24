56f357e5578d9dd143c2a7186665fa0c8c9eeffe
Nuno Martins <nunomartins@snbere96p43jn4.lan>
2026-02-24 13:13:57 +0000

    illuminate

--- lua/packages.lua
+++ lua/packages.lua
@@ -1,23 +1,24 @@
-vim.pack.add({
-    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
-    { src = "https://github.com/rebelot/kanagawa.nvim" },
-    { src = "https://github.com/nvim-mini/mini.nvim" },
+vim.pack.add {
+    { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+    { src = 'https://github.com/rebelot/kanagawa.nvim' },
+    { src = 'https://github.com/nvim-mini/mini.nvim' },
     -- Find
-    { src = "https://github.com/mikavilpas/yazi.nvim" },
-    { src = "https://github.com/nvim-lua/plenary.nvim" },
-    { src = "https://github.com/ibhagwan/fzf-lua" },
-    { src = "https://github.com/MagicDuck/grug-far.nvim" },
+    { src = 'https://github.com/mikavilpas/yazi.nvim' },
+    { src = 'https://github.com/nvim-lua/plenary.nvim' },
+    { src = 'https://github.com/ibhagwan/fzf-lua' },
+    { src = 'https://github.com/MagicDuck/grug-far.nvim' },
     -- LSP
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/folke/lazydev.nvim" },
-    { src = "https://github.com/saghen/blink.cmp" },
-    { src = "https://github.com/stevearc/conform.nvim" },
-    { src = "https://github.com/linux-cultist/venv-selector.nvim" },
+    { src = 'https://github.com/mason-org/mason.nvim' },
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/folke/lazydev.nvim' },
+    { src = 'https://github.com/saghen/blink.cmp' },
+    { src = 'https://github.com/stevearc/conform.nvim' },
+    { src = 'https://github.com/linux-cultist/venv-selector.nvim' },
     -- Treesitter
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter"},
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
+    { src = 'https://github.com/RRethy/vim-illuminate' },
     -- Git
-    { src = "https://github.com/lewis6991/gitsigns.nvim" },
+    { src = 'https://github.com/lewis6991/gitsigns.nvim' },
     -- MD
-    { src = "https://github.com/OXY2DEV/markview.nvim" },
-})
+    { src = 'https://github.com/OXY2DEV/markview.nvim' },
+}
