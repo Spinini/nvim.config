3d7b10ecad061056c5460341f7a91b07475cf0f3
Spin <spnispni9@gmail.com>
2026-03-10 20:47:33 +0000

    icons

--- lua/plugins/lang/blink.lua
+++ lua/plugins/lang/blink.lua
@@ -1,34 +1,14 @@
 require('blink-cmp').setup {
     fuzzy = { implementation = 'lua' },
     ghost_text = { enabled = true },
+    snippets = { preset = 'luasnip' },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
     completion = {
         trigger = {
             show_on_trigger_character = true,
             show_on_blocked_trigger_characters = { ' ', '\n', '\t' },
-        },
-        menu = {
-            draw = {
-                components = {
-                    kind_icon = {
-                        text = function(ctx)
-                            local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
-                            return kind_icon
-                        end,
-                        -- (optional) use highlights from mini.icons
-                        highlight = function(ctx)
-                            local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                            return hl
-                        end,
-                    },
-                    kind = {
-                        -- (optional) use highlights from mini.icons
-                        highlight = function(ctx)
-                            local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                            return hl
-                        end,
-                    },
-                },
-            },
         },
     },
     keymap = {
@@ -48,10 +28,10 @@ require('blink-cmp').setup {
         ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
         ['<Up>'] = { 'select_prev', 'fallback' },
         ['<Down>'] = { 'select_next', 'fallback' },
-        ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
-        ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+        ['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
+        ['<C-j>'] = { 'select_next', 'fallback_to_mappings' },
         ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
         ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+        ['<C-p>'] = { 'show_signature', 'hide_signature', 'fallback' },
     },
 }
