3db48dbeb530865d1f01cd9bd6067f5f588354be
Spin <>
2026-04-16 14:27:59 +0100

    docker

--- lua/plugins/lang/tools.lua
+++ lua/plugins/lang/tools.lua
@@ -13,6 +13,10 @@ ensure_installed 'gersemi'
 vim.lsp.enable { 'neocmake' }
 require('conform').formatters_by_ft.cmake = { 'gersemi' }
 
+-- Docker
+ensure_installed 'docker-language-server'
+vim.lsp.enable { 'docker_language_server' }
+
 -- Json
 ensure_installed 'jq'
 require('conform').formatters_by_ft.json = { 'jq' }
